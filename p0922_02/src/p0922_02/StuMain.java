51cc804f0dd2a7f6e7ed223519b8e68b852fe1c4
KOREAVC <KOREAVC@DESKTOP-OO4UAMB>
2023-09-22 18:53:19 +0900

    학생성적 입력,출력,수정,정렬

---
+++ p0922_02/src/p0922_02/StuMain.java
@@ -0,0 +1,24 @@
+package p0922_02;
+
+public class StuMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("---------------------------------");
+		System.out.println("[[ 학생성적처리 프로그램 ]]");
+		System.out.println("---------------------------------");
+		System.out.println("1. 학생성적입력");
+		System.out.println("2. 학생성적출력");
+		System.out.println("3. 학생성적수정");
+		System.out.println("4. 학생성적삭제");
+		System.out.println("5. 이름순 정렬");
+		System.out.println("6. 성적높은순 정렬");
+		System.out.println("7. 성적낮은순 정렬");
+		System.out.println("8. 파일가져오기");
+		System.out.println("9. 파일저장하기");
+		System.out.println("10. 등수처리");
+		System.out.println("0. 프로그램종료");
+
+	}
+
+}
